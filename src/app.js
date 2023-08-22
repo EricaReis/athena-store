ec1922defae6dcdaee4ecd2bd3f2fc3d54101783
EricaReis <ericareis67@outlook.com>
2023-08-22 20:22:03 -0300

    Add color variables

--- src/app.js
+++ src/app.js
@@ -24,3 +24,19 @@ const swiper = new Swiper('.swiper', {
   },
 });
 
+/* Populate Products list section */
+$.ajax({
+  url: 'https://fakestoreapi.com/products/category/electronics',
+}).done(function (data) {
+  for (var i = 0; i < data.length; i++) {
+    var item = data[i];
+    var imageId = $('#eletronic-img-'+i);
+    var titleId = $('#eletronic-title-'+i);
+    var priceId = $('#eletronic-price-'+i);
+
+    $(imageId).attr('src', item.image);
+    $(titleId).text(item.title.substring(0, 30) + "...");
+    $(priceId).text('$'+item.price.toFixed(2));
+    console.log(item)
+  }
+});
