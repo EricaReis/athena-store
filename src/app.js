068614832c69b61bb343a3dcab9f807d8cae89a6
EricaReis <ericareis67@outlook.com>
2023-08-23 09:55:16 -0300

    Add scroll animation

--- src/app.js
+++ src/app.js
@@ -14,6 +14,17 @@ $.ajax({
   }
 });
 
+/* Scroll animation */
+$('nav a').click(function(e) {
+  e.preventDefault();
+  var id = $(this).attr('href'),
+    targetOffset = $(id).offset().top;
+
+  $('html, body').animate({
+    scrollTop: targetOffset
+  }, 500);
+});
+
 /* Carousel Config */
 const swiper = new Swiper('.swiper', {
   direction: 'horizontal',
